7851e1faa88ab38c36cfa9ae91a8e66056c10eae
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-24 15:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+const select = document.getElementById("colorSelect");
+
+const getButton = document.querySelector("form > input");
+
+getButton.addEventListener("click", (event) => {
+  for (let i = 0; i < select.children.length; i++) {
+    if (select.children[i].value === select.value) {
+      select.removeChild(select.children[i]);
+    }
+  }
+});
